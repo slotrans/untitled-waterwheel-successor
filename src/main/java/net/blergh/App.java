011bf8e398c1c6962a325ecea6b476194742254b
Noah Yetter <nyetter@gmail.com>
2023-07-09 15:02:55 -0600

    removed some cruft, made import graph method useful

--- src/main/java/net/blergh/App.java
+++ src/main/java/net/blergh/App.java
@@ -8,17 +8,13 @@ import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
-import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
 import org.jgrapht.graph.*;
-import org.jgrapht.graph.builder.GraphBuilder;
 import org.jgrapht.nio.dot.DOTImporter;
 import org.slf4j.Logger;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.StringReader;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -27,6 +23,15 @@ import java.util.*;
 public class App
 {
     private static final Logger log = LoggerFactory.make();
+
+    private static final String SRC = "src";
+    private static final String SOURCE_RELATIONS_DOT_GV = SRC+"/source_relations.gv";
+    private static final String CREATE_DOT_SQL = "create.sql";
+    private static final String BUILD_DOT_SQL = "build.sql";
+    private static final String DEPS_DOT_GV = "deps.gv";
+    private static final String CONFIG_DOT_PROPERTIES = "build.properties";
+    private static final String BUILD_MODE_PROP = "buildMode";
+
     public static void main( String[] args ) throws Exception
     {
         ArgumentParser parser = ArgumentParsers.newFor("untitled-waterwheel-successor").build().description("BUILD SOME TABLES WOOOOO");
@@ -56,12 +61,14 @@ public class App
         final DirectedAcyclicGraph<RelName, DefaultEdge> nameDAG = new DirectedAcyclicGraph<>(DefaultEdge.class);
 
         //seed the dependency graph with declared sources
-        //TODO: import source_tables.gv into the above graph
+        final DirectedAcyclicGraph<RelName, DefaultEdge> sourceRelations = importNameDAGFragment(Path.of(SOURCE_RELATIONS_DOT_GV));
+        Graphs.addAllVertices(nameDAG, sourceRelations.vertexSet());
 
         //keep track of all tasks by their name
         final Map<RelName, Task> taskTable = new HashMap<>();
 
         nameDAG.forEach( nodeRelName -> {
+            log.info("constructing SourceTask for {}", nodeRelName.toFullName());
             SourceTask sourceTask = new SourceTask(nodeRelName);
             taskTable.put(nodeRelName, sourceTask);
         });
@@ -69,7 +76,7 @@ public class App
         //find all src/<schema>/<table>/ directories
         log.debug("enumerating script directories...");
         final List<Path> srcDirectoryPaths = Files.find(
-                    Path.of("src"),
+                    Path.of(SRC),
                     3,
                     (path, basicFileAttributes) -> basicFileAttributes.isDirectory(),
                     FileVisitOption.FOLLOW_LINKS
@@ -92,29 +99,29 @@ public class App
             String buildScript = "";
             String depsFragment = "";
             Properties config = new Properties();
-            config.put("buildMode", BuildMode.FULL.toString());
+            config.put(BUILD_MODE_PROP, BuildMode.FULL.toString());
 
             for(Path filePath : Files.list(tableDirectoryPath).toList() )
             {
-                if( "create.sql".equals(filePath.getFileName().toString()) )
+                if( CREATE_DOT_SQL.equals(filePath.getFileName().toString()) )
                 {
                     hasCreate = true;
                     createScript = Files.readString(filePath);
                 }
 
-                if( "build.sql".equals(filePath.getFileName().toString()) )
+                if( BUILD_DOT_SQL.equals(filePath.getFileName().toString()) )
                 {
                     hasBuild = true;
                     buildScript = Files.readString(filePath);
                 }
 
-                if( "deps.gv".equals(filePath.getFileName().toString()) )
+                if( DEPS_DOT_GV.equals(filePath.getFileName().toString()) )
                 {
                     hasDeps = true;
                     depsFragment = Files.readString(filePath);
                 }
 
-                if( "config.properties".equals(filePath.getFileName().toString()) )
+                if( CONFIG_DOT_PROPERTIES.equals(filePath.getFileName().toString()) )
                 {
                     Properties fileProps = new Properties();
                     fileProps.load(Files.newBufferedReader(filePath));
@@ -134,7 +141,7 @@ public class App
                     relName,
                     createScript,
                     buildScript,
-                    BuildMode.valueOf((String) config.get("buildMode")),
+                    BuildMode.valueOf((String) config.get(BUILD_MODE_PROP)),
                     null //TODO
             );
             taskTable.put(relName, sqlTask);
@@ -165,91 +172,17 @@ public class App
 //        }
     }
 
-    private static void importGraphFromFile()
+    private static DirectedAcyclicGraph<RelName, DefaultEdge> importNameDAGFragment(Path path) throws IOException
     {
-        Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
-        DOTImporter<String, DefaultEdge> importer = new DOTImporter<>();
-        importer.setVertexFactory(x -> x); // read the vertex name as a string, return it unmodified
+        final DirectedAcyclicGraph<RelName, DefaultEdge> graph = new DirectedAcyclicGraph<>(DefaultEdge.class);
+        final DOTImporter<RelName, DefaultEdge> importer = new DOTImporter<>();
+        importer.setVertexFactory(RelName::fromString);
 
-        System.out.println("importing graph from complete_deps.gv");
-        try (Reader reader = new FileReader("complete_deps.gv"))
-        {
-            importer.importGraph(graph, reader);
+        final String fileContents = Files.readString(path);
+        final String wrapped = "digraph {\n" + fileContents + "\n}";
 
-            System.out.println("edges:");
-            for(DefaultEdge edge : graph.edgeSet())
-            {
-                System.out.println(edge.toString());
-            }
-        }
-        catch (FileNotFoundException e)
-        {
-            throw new RuntimeException(e);
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
+        importer.importGraph(graph, new StringReader(wrapped));
+        return graph;
     }
 
-    private static void walkSrcDir() throws IOException
-    {
-        final List<Path> srcPaths = Files.find(
-                Path.of("src"),
-                3,
-                (path, basicFileAttributes) -> basicFileAttributes.isDirectory(),
-                FileVisitOption.FOLLOW_LINKS
-        ).toList();
-
-        for( Path tableDirectoryPath : srcPaths )
-        {
-            // the only split length we're interested in is 3, e.g. src/schemaname/tablename -> ['src', 'schemaname', 'tablename']
-            // src and src/schemaname (split < 3)are just ignored
-            // src/schemaname/tablename/junk (split > 3) is permitted, if users want to store notes, scratch code, design docs, whatever
-            if( tableDirectoryPath.getNameCount() != 3)
-            {
-                log.debug("ignoring directory {}", tableDirectoryPath);
-                continue;
-            }
-
-            final String schemaName = tableDirectoryPath.getName(1).toString();
-            final String tableName = tableDirectoryPath.getName(2).toString();
-            final RelName relName = new RelName(schemaName, tableName);
-            //TODO: equivalent of BUILD_SCOPE.add(node_name)
-
-            final List<Path> filesInDir = Files.list(tableDirectoryPath)
-                    .map(Path::getFileName)
-                    .toList();
-            final boolean hasCreate = filesInDir.contains(Path.of("create.sql"));
-            final boolean hasBuild = filesInDir.contains(Path.of("build.sql"));
-            final boolean hasDeps = filesInDir.contains(Path.of("deps.gv"));
-
-            if( hasCreate && hasBuild && hasDeps )
-            {
-                // this is what we want
-                log.debug("{} has all necessary scripts", tableDirectoryPath);
-            }
-            else if( !hasCreate && !hasBuild && !hasDeps )
-            {
-                log.warn("{} has no scripts, ignoring", tableDirectoryPath);
-                //TODO: equivalent of BUILD_SCOPE.remove(node_name)
-            }
-            else if( hasCreate && hasBuild && !hasDeps )
-            {
-                // is this right? i don't think it is...
-                // if deps.gv exists but is empty, that would logically be equivalent, but would not fall into this branch
-                // also if another table declares a dependency on this table, that would put it in the graph
-                log.warn("{} missing deps.gv, table may only be built directly", tableDirectoryPath);
-
-                //2023-07-02:
-                //  I don't think this IS right, in the new design.
-                //  In general it should be ok to have a table with no dependencies, even if that's weird.
-                //  We may need to take additional steps to ensure such tables end up in the graph.
-            }
-            else
-            {
-                log.error("{} missing sufficient scripts, any build involving it will fail", tableDirectoryPath);
-            }
-        }
-    }
 }
